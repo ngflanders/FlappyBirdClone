67173e5d4de3640f17c80e66d0124f43621990e6
Nick <ngflanders@gmail.com>
2015-12-28 16:55:46 -0800

    Added double buffering

--- src/BirdApplet.java
+++ src/BirdApplet.java
@@ -12,6 +12,9 @@ public class BirdApplet extends Applet implements Runnable {
     int y_pos = 100;
     int radius = 20;
 
+    private Image dbImage;
+    private Graphics dbg;
+
     public void run() {
         Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
 
@@ -23,6 +26,7 @@ public class BirdApplet extends Applet implements Runnable {
 
             try {
                 Thread.sleep(20);
+                //throw new InterruptedException();
             } catch (InterruptedException e) {
                 // nothing
             }
@@ -37,6 +41,22 @@ public class BirdApplet extends Applet implements Runnable {
     public void start() {
         Thread th = new Thread(this);
         th.start();
+    }
+
+    public void update(Graphics g) {
+        if (dbImage == null) {
+            dbImage = createImage(this.getSize().width, this.getSize().height);
+            dbg = dbImage.getGraphics();
+
+            dbg.setColor(getBackground());
+            dbg.fillRect(0,0,this.getSize().width, this.getSize().height);
+
+            dbg.setColor(getForeground());
+            paint(dbg);
+
+            g.drawImage(dbImage,0,0, this);
+
+        }
     }
 
     public void stop() { }
--- src/TestClass.java
+++ src/TestClass.java
@@ -6,7 +6,7 @@
 public class TestClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Sup");
+        System.out.println("Hello World -Nick");
+        System.out.println("Hi -Lee");
     }
 }
